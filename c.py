ed67249254389506c4f5e60d71de84addef4e3c3
Bombastrelopas <ioannis8@hotmail.com>
2016-12-19 01:48:49 +0200

    v0.02

--- c.py
+++ c.py
@@ -1,5 +1,6 @@
 import discord
 import random
+import collections
 
 
 client = discord.Client()
@@ -7,6 +8,20 @@ cards = [ 'A', '1', '2', '3' , '4' , '5' , '6' , '7' , '8' , '9' , '10' , 'J' , 
 mothercards = []
 player1_cards = []
 
+
+class Deck:
+    ranks=[string(n) for n in range(2,12)] + list('JQKA')
+    suits= 'spathia karo kupes mpastunia'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank,suit) for suit in self.suits
+                for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self,position):
+        return self._cards[position]
 
 @client.event
 async def on_message(message):
@@ -34,20 +49,15 @@ async def on_message(message):
             await client.send_message(message.channel, 'Sorry. It is actually {}.'.format(answer))
 
     if message.content.startswith('$game'):
-        #Pick a random card from deck
-        def choose_random_card():
-            return cards[random.randint(0, len(cards)-1)]
-
-        #check if user typed hit or stand
+        deck=Deck();
         def choice_check(m):
             if (m == 'hit' or  m == 'stand'):
                 return m;
             return m;
 
 
-        await client.send_file(message.channel , '/Users/john/Desktop/Projects/DiscordBot/Pics/blackjackgirl.jpg' )
-        await client.send_message(message.channel , 'Kalispera kukle/kukla as paiksume blackjack!')
-        await client.send_message(message.channel , 'Exw \n' +choose_random_card())
+        await client.send_message(message.channel , "Let's play blackjack!")
+        await client.send_message(message.channel , 'I have \n' +choose_random_card())
 
         await client.send_message(message.channel , 'Exeis \n' +choose_random_card() + ' kai ' +choose_random_card())
         await client.send_message(message.channel , 'Γράψε <<hit>> για να χτυπήσεις ή <<stand>> για να μείνεις')
