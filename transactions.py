af7798692877d009402190773cba4ae8e32663f0
John Miragias <John Miragias>
2017-03-25 23:02:02 +0200

    Added TODO stuff

--- transactions.py
+++ transactions.py
@@ -16,3 +16,7 @@ async def TellUserMoney(message):
     await client.send_message(message.channel, 'User {} has {} marks'.format(row1[0], row1[1]))
     conn.close()
     return
+
+# TODO : Check if user is in database
+# TODO : Add or Remove X Value From User
+# TODO : Add user to database IF he wants to and is not already in
